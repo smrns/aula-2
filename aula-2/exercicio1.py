666fbdd48ce5e3eb13cc531a03acfea59fe763ca
smrns <sabrinamarins06@gmail.com>
2026-04-02 21:25:24 -0300

    fiz dois exercicios em python relembrando a aula passada e treinando o github

---
+++ aula-2/exercicio1.py
@@ -0,0 +1,10 @@
+# Exercicios da aula 03 - dia 02/04/2026 - testando o git
+num = int(input('Digite um número, vou lhe mostrar o dobro, triplo e o seu quadrado: '))
+dobro = num * 2
+triplo = num * 3
+quadrado = num * 4
+
+print(f'Aqui está o número escolhido: {num}')
+print(f'Aqui está o dobro dele: {dobro}')
+print(f'Aqui está o triplo dele: {triplo}')
+print(f'Aqui está o quadrado dele: {quadrado}')
---
+++ aula-2/exercicio2.py
@@ -0,0 +1,9 @@
+# Exercicios da aula 03 - dia 02/04/2026 - testando o git
+num1 = float(input('Digite sua 1º nota: '))
+num2 = float(input('Digite sua 2º nota: '))
+num3 = float(input('Digite sua 3º nota: '))
+num4 = float(input('Digite sua 4º nota: '))
+
+media = (num1 + num2 + num3 + num4)/4
+
+print(f'Sua média é: {media}')
