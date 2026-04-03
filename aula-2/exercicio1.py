c6d13a6405285ee1d8eb334fffaa53f12533166b
smrns <sabrinamarins06@gmail.com>
2026-04-02 21:48:10 -0300

    fiz uma alteração no exercicio1, corrigi o quadrado do número

--- aula-2/exercicio1.py
+++ aula-2/exercicio1.py
@@ -2,7 +2,7 @@
 num = int(input('Digite um número, vou lhe mostrar o dobro, triplo e o seu quadrado: '))
 dobro = num * 2
 triplo = num * 3
-quadrado = num * 4
+quadrado = num ** 2
 
 print(f'Aqui está o número escolhido: {num}')
 print(f'Aqui está o dobro dele: {dobro}')
